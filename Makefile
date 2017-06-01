73b7233e71dcc757f4a3f6aa1f56e021d8f92423
yamadapc <tacla.yamada@gmail.com>
2017-06-01 00:13:46 -0300

    Workspace update

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ uninstall:
 push:
 	git push gitlab master
 	git push origin master
+
+docker:
+	docker build -f ./fpm.dockerfile -t beijaflorio/fpm .
+	docker build -f ./stack-fpm-ci.dockerfile -t beijaflorio/stack-fpm-ci .
+	docker push beijaflorio/fpm
+	docker push beijaflorio/stack-fpm-ci
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
 import           Protolude
 
+import           Control.Arrow
+import qualified Data.List as List
 import           Options.Applicative
+import           System.Directory
+import qualified System.FilePath as FilePath
 import           System.Directory.ProjectRoot
-import           Turtle                       hiding (switch)
+import           Turtle                       hiding (switch, find)
+
+import           Distribution.PackageDescription.Parse
+import qualified Data.Yaml as Yaml
 
 main :: IO ()
 main = do
@@ -16,14 +24,14 @@ main = do
                                   header "stack-fpm"))
     runWithOptions opts
   where
-    opts = StackFpmOptions <$> (toS <$> strOption (long "name"
+    opts = StackFpmOptions <$> (fmap toS <$> optional (strOption (long "name"
                                                        <> metavar "NAME"
-                                                       <> help "The name of the package to build"))
+                                                       <> help "The name of the package to build")))
                            <*> switch (long "verbose"
                                            <> help "Be verbose")
 
-runWithOptions _ = do
-    echo "Hello"
+runWithOptions StackFpmOptions{..} = do
+    name <- fromMaybe getName (return <$> stackFpmName)
     Turtle.stdout (inshell "stack docker pull" empty)
     Turtle.stdout (inshell "stack build --docker" empty)
 
@@ -33,9 +41,7 @@ buildForDarwin = undefined
 runFpm = undefined
 
 type FpmTargetType = Text
-
 type FpmSourceType = Text
-
 data FpmOptions = FpmOptions { fpmTarget        :: FpmTargetType
                              , fpmSource        :: FpmSourceType
                              , fpmDebPreDepends :: [Text]
@@ -44,13 +50,31 @@ data FpmOptions = FpmOptions { fpmTarget        :: FpmTargetType
                              , fpmInput         :: Turtle.FilePath
                              }
 
+getName = do
+    cwd <- getCurrentDirectory
+    fs <- getDirectoryContents cwd
+    case List.find (== "package.yaml") fs of
+        Just hpackManifest ->
+            hpackGetName hpackManifest
+        Nothing -> case List.find (FilePath.takeExtension >>> (== ".cabal")) fs of
+            Just cabalManifest ->
+                cabalGetName cabalManifest
+            Nothing -> error "Can't get default name (no cabal/hpack manifest)"
+  where
+    cabalGetName fp = do
+        f <- readFile fp
+        case parseGenericPackageDescription f of
+            Right (GenericPackageDescription{..}) -> undefined
+            _ -> undefined
+    hpackGetName = undefined
+
 getVersion = undefined
 
 tar = undefined
 
 stackBuild = undefined
 
-data StackFpmOptions = StackFpmOptions { stackFpmName    :: Text
+data StackFpmOptions = StackFpmOptions { stackFpmName    :: Maybe Text
                                        , stackFpmVerbose :: Bool
                                        }
   deriving(Show)
